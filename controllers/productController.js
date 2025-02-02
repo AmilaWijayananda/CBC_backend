add0563c7be886fe71595e02ca1117918e0141b6
AmilaWijayananda <amisampath12@gmail.com>
2025-02-02 09:28:37 +0530

    add error status ✅

--- controllers/productController.js
+++ controllers/productController.js
@@ -63,7 +63,7 @@ export function createProduct(req,res){
       message: "Product created"
     })
   }).catch((error)=>{
-    res.json({
+    res.status(403).json({
       message: error
     })
   })
