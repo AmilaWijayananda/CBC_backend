80bb8b8d178d1823fb95c7075522caf348f4c8a3
AmilaWijayananda <amisampath12@gmail.com>
2025-02-02 19:04:55 +0530

    Change product router delet part ✅

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -5,7 +5,7 @@ const productRouter = express.Router();
 
 productRouter.get('/',getProduct);     //link : localhost:5000/api/products (normal GET request)
 productRouter.post('/',createProduct);  //link : localhost:5000/api/products (normal POST request)
-productRouter.delete("/",deleteProduct);  //link : localhost:5000/api/products (normal DELETE request)
+productRouter.delete("/:productId",deleteProduct);  //link : localhost:5000/api/products ( DELETE by Id request)
 productRouter.get("/ByName",getProductByName);  //link : localhost:5000/api/products/ByName (Special GET request name attached to json body)
 productRouter.get("/:name",getProductByLinkName);  //link : localhost:5000/api/products/Tab A9 (Special GET request namme attached with link)
 
