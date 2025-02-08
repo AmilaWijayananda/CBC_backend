4bfea661cf2bcb4a35feee76889080c8291a4452
AmilaWijayananda <amisampath12@gmail.com>
2025-02-08 15:41:41 +0530

    sort by orderId ✅

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -15,7 +15,7 @@ export async function createOrder(req,res){
 
     try{
         const latestOrder = await Order.find().sort
-        ({date : -1}).limit(1)
+        ({orderId : -1}).limit(1)
 
         let orderId
 
