382790b636b4b4858d3fa5eee82ac8112727e58a
AmilaWijayananda <amisampath12@gmail.com>
2025-02-12 11:36:17 +0530

    add search route ✅

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { createProduct, deleteProduct, getProduct, getProductByName, getProductByLinkName, updateProduct, getProductById } from '../controllers/productController.js';
+import { createProduct, deleteProduct, getProduct, getProductByName, getProductByLinkName, updateProduct, getProductById, searchProducts } from '../controllers/productController.js';
 
 const productRouter = express.Router();
 
 productRouter.get('/',getProduct);     //link : localhost:5000/api/products (normal GET request)
 productRouter.post('/',createProduct);  //link : localhost:5000/api/products (normal POST request)
+productRouter.get("/search/:query",searchProducts);
 productRouter.delete("/:productId",deleteProduct);  //link : localhost:5000/api/products ( DELETE by Id request)
 productRouter.get("/ByName",getProductByName);  //link : localhost:5000/api/products/ByName (Special GET request name attached to json body)
 //productRouter.get("/:name",getProductByLinkName);  //link : localhost:5000/api/products/Tab A9 (Special GET request namme attached with link)
